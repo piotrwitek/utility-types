0df38edbcbd6c1a379ccc8d3e6d3398762b199f7
Piotr Witek <piotrek.witek@gmail.com>
2016-12-14 01:00:05 +0100

    added assign helper class

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,9 @@
  * @author Piotr Witek <piotrek.witek@gmail.com> (http://piotrwitek.github.io)
  * @copyright Copyright (c) 2016 Piotr Witek
  * @license MIT
+ */
+
+/**
  * @export
  * @template S - State
  * @template P - Props
@@ -11,3 +14,14 @@
 export function getStateProps<S, P>(mapStateToProps: (state: S) => P): P {
   return {} as false || mapStateToProps({} as S);
 }
+
+/**
+ * @export
+ * @template S - State
+ * @template P - Props
+ * @param {(state: S) => P} mapStateToProps
+ * @returns {P}
+ */
+// declare class Object {
+//   assign<T>(target: T, ...sources: Partial<T>[]): T;
+// }
