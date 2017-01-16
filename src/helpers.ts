ffad74110b3154f94acf51850d39563102a6ea8e
Piotr Witek <piotrek.witek@gmail.com>
2017-01-16 02:04:22 +0100

    added create Empty Action

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,25 +1,22 @@
-import { IAction } from './types';
+import { IEmptyAction, IAction } from './types';
 
 /**
- * @export createAction - FSA Actions constructor function
+ * @export createAction - empty action creator constructor function
+ * @template T - Type
+ * @param {T} type
+ * @returns () => IEmptyAction<T>
+ */
+export function createEmptyAction<T>(type: T): () => IEmptyAction<T> {
+  return () => ({ type });
+};
+
+/**
+ * @export createFSAction - FSA action creator constructor function
  * @template T - Type
  * @template P - Payload
  * @param {T} type
- * @returns (payload?: P): IAction<T, P>
+ * @returns (payload?: P) => IAction<T, P>
  */
-export function createAction<T, P>(type: T) {
-  return (payload: P): IAction<T, P> => ({ type, payload });
-}
-
-
-/**
- * @export returntypeof - extract return type of "expression"
- * workaround until added support in TS
- * https://github.com/Microsoft/TypeScript/issues/6606
- * @template RT - Return Type
- * @param {(...params: any[]) => RT} expression
- * @returns {RT}
- */
-export function returntypeof<RT>(expression: (...params: any[]) => RT): RT {
-  return {} as RT;
+export function createAction<T, P>(type: T): (payload: P) => IAction<T, P> {
+  return (payload) => ({ type, payload });
 }
