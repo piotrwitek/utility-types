a1c2279107ecae0937c334385a3d77cc7800e109
Piotr Witek <piotrek.witek@gmail.com>
2018-12-26 11:37:40 +0100

    Updated functional-helpers test

--- src/functional-helpers.spec.snap.ts
+++ src/functional-helpers.spec.snap.ts
@@ -1,8 +1,7 @@
 import { getReturnOfExpression } from './functional-helpers';
 
 // @dts-jest:group getReturnOfExpression
-it('getReturnOfExpression', () => {
-  const increment = () => ({ type: 'INCREMENT' as 'INCREMENT' });
-  // @dts-jest:pass:snap -> { type: "INCREMENT"; }
-  getReturnOfExpression(increment); // => undefined
-});
+
+const increment = () => ({ type: 'INCREMENT' as 'INCREMENT' });
+// @dts-jest:pass:snap -> { type: "INCREMENT"; }
+getReturnOfExpression(increment); // => undefined
