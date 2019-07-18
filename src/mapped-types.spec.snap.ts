7cc4156756c07936096562824e705045badc9e6b
xuyx001 <xuyx001@cmft.com>
2019-07-18 11:19:59 +0800

    update snapshot

--- src/mapped-types.spec.snap.ts
+++ src/mapped-types.spec.snap.ts
@@ -116,13 +116,13 @@ type RequiredOptionalProps = {
 
 // @dts-jest:group FunctionKeys
 {
-  // @dts-jest:pass:snap -> "setName | someFn "
+  // @dts-jest:pass:snap -> "setName" | "someFn"
   testType<FunctionKeys<MixedProps>>();
 }
 
 // @dts-jest:group NonFunctionKeys
 {
-  // @dts-jest:pass:snap -> "name | someKeys"
+  // @dts-jest:pass:snap -> "name" | "someKeys"
   testType<NonFunctionKeys<MixedProps>>();
 }
 
